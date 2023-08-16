346bba132072a212eff7018a72e1b0a8e0554760
unknown <erukaemmanuel98@gmail.com>
2023-08-16 01:10:59 +0100

    updated user profile component!

--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -3,7 +3,7 @@ import './UserProfile.css';
 const UserProfile = (props) => {
     return (
         <div className="user-profile fillet-border-sm-App light-border-outline-App px-2 py-3">
-            <h5>User Profile</h5>
+            <h5>User Profile Component!</h5>
         </ div>
     )
 }
